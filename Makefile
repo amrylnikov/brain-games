1e609c37207bb04bef82f6778f9e0af590fa44dc
amrylnikov <amrylnikov@gmail.com>
2023-03-06 15:44:05 +0300

    done lvl 4

--- Makefile
+++ Makefile
@@ -12,3 +12,6 @@ publish:
 
 pakage-install:
 	python3 -m pip install --user dist/*.whl
+
+lint:
+	poetry run flake8 brain_games
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-import prompt
 import os
 import sys
 import inspect
 
-#sys.path.insert(1, r'C:\Users\Алексей\PycharmProjects\python-project-49\brain_games')
-#from cli import welcome_user
-
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(os.path.abspath
+                             (inspect.getfile
+                              (inspect.currentframe(
+                              ))))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-import cli
+from cli import welcome_user
+
 
 def main():
     print('Welcome to the Brain Games!')
-    cli.welcome_user()
+    welcome_user()
+
 
 if __name__ == '__main__':
     main()
