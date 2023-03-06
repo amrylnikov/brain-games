e9d03f2a913ee890fb9d96338fddc4edc3f4e25e
amrylnikov <amrylnikov@gmail.com>
2023-03-06 16:20:11 +0300

    step 5 is done

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/amrylnikov/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/amrylnikov/python-project-49/actions)
 
+Linter:
 <a href="https://codeclimate.com/github/amrylnikov/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/c448c1161a3c051e1460/maintainability" /></a>
+
+Video of working process:
+https://asciinema.org/a/eAjq1p5b1i4V1iLEaUGuoZL8M
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os
+import sys
+import inspect
+import random
+import prompt
+
+currentdir = os.path.dirname(os.path.abspath
+                             (inspect.getfile
+                              (inspect.currentframe(
+                              ))))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from cli import welcome_user
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    counter = 0
+    while counter < 3:
+        number = random.randint(0,100)
+        if number % 2 == 0:
+            true_answer = 'yes'
+        else:
+            true_answer = 'no'
+        print('Question: ', number)
+        answer = prompt.string('Your answer: ')
+        if answer == true_answer:
+            print('Correct!')
+            counter += 1
+        else:
+            print('\'', answer, '\' is wrong answer ;(. Correct answer was \'', true_answer, '\'.', sep = '')
+
+
+if __name__ == '__main__':
+    main()
