5edd67755f97e15fb5a3621bcff75fc0b7b99cf6
amrylnikov <amrylnikov@gmail.com>
2023-08-11 12:34:20 +0300

    creared a few lined in brain-calc

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,17 +1,4 @@
-#!/usr/bin/env python3
-import os
-import sys
-import inspect
 import prompt
-
-currentdir = os.path.dirname(os.path.abspath
-                             (inspect.getfile
-                              (inspect.currentframe(
-                              ))))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
-# from cli import welcome_user
 
 
 def welcome_user():
