dfe7073e98c6e9021ab218586576c63c1e8aee93
amrylnikov <amrylnikov@gmail.com>
2023-08-12 10:56:56 +0300

    fixed engine

--- brain_games/scripts/engine.py
+++ brain_games/scripts/engine.py
@@ -7,23 +7,16 @@ def run(condition_output, generation_func):
     name = prompt.string('May I have your name? ')
     print('Hello,', name)
     print(condition_output)
-    counter = 0
-    # вместо while -> for i in range 3
-    while counter < 3:
+    for _ in range(3):
         question, true_answer = generation_func()
         print('Question:', question)
-        # без инпут тайпа, всегда стринг передавай и выдавай
         answer = prompt.string('Your answer: ')
         if answer == true_answer:
             print('Correct!')
-            counter += 1
         else:
-            # переписать в строковом варианте (f который)
-            print('\'', answer,
-                  '\' is wrong answer ;(. Correct answer was \'',
-                  true_answer, '\'.',
-                  # И это вторым принтом через f, ибо читается проще
-                  '\nLet\'s try again, ', name, '!', sep='')
-            break
-    if counter == 3:
-        print('Congratulations, ', name, '!', sep='')
+            return print(
+                f"\'{answer}\' is wrong answer ;(. Correct answer was "
+                f"\'{true_answer}\'."
+                f"\nLet\'s try again, {name}!"
+            )
+    print('Congratulations, ', name, '!', sep='')
